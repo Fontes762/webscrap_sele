47670300fd49e501ad8da9cbcbea59b732cb89c8
Fontes762 <pedrotelheirofontes@gmail.com>
2025-09-29 11:24:39 -0300

    Update salva_bd.py

--- salva_bd.py
+++ salva_bd.py
@@ -1,1 +1,32 @@
+from __future__ import print_function
+from datetime import date, datetime, timedelta
 import mysql.connector
+import json
+
+cnx = mysql.connector.connect(user='', database='produtos_db')
+cursor = cnx.cursor()
+
+tomorrow = datetime.now().date() + timedelta(days=1)
+
+add_produto = ("INSERT INTO produtos "
+               "(nome, preco, mercado) "
+               "VALUES (%s, %s, %s)")
+
+
+with open('paodeacucar_all_pages.json', 'r',encoding='utf-8') as arquivo:
+    dados = json.load(arquivo)
+
+
+# Insert new employee
+cursor.execute(add_produto)
+emp_no = cursor.lastrowid
+
+# Insert salary information
+
+
+
+# Make sure data is committed to the database
+cnx.commit()
+
+cursor.close()
+cnx.close()
