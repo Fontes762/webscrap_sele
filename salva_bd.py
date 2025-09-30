b5ca1696d43610a6e1c4d14b2a420d03967446bf
Fontes762 <pedrotelheirofontes@gmail.com>
2025-09-30 11:03:01 -0300

    Update salva_bd2.py

--- salva_bd.py
+++ salva_bd.py
@@ -3,10 +3,23 @@ from datetime import date, datetime, timedelta
 import mysql.connector
 import json
 
+
+cnx = mysql.connector.connect(
+    user="root",
+    password="",
+    host="127.0.0.1",
+    port=3306,  # <-- ajuste se a porta for outra
+    database="produtos_db"
+)
+cursor = cnx.cursor()
+
+tomorrow = datetime.now().date() + timedelta(days=1)    
+
 cnx = mysql.connector.connect(user='', database='produtos_db')
 cursor = cnx.cursor()
 
 tomorrow = datetime.now().date() + timedelta(days=1)
+
 
 add_produto = ("INSERT INTO produtos "
                "(nome, preco, mercado) "
@@ -17,8 +30,21 @@ with open('paodeacucar_all_pages.json', 'r',encoding='utf-8') as arquivo:
     dados = json.load(arquivo)
 
 
+
+    for produto in dados:
+        info_produto = (
+            produto["nome"],
+            produto["preco"],
+            produto["mercado"]
+        )
+        cursor.execute(add_produto,info_produto)
+
+# Insert new employee
+
+
 # Insert new employee
 cursor.execute(add_produto)
+
 emp_no = cursor.lastrowid
 
 # Insert salary information
@@ -27,6 +53,11 @@ emp_no = cursor.lastrowid
 
 # Make sure data is committed to the database
 cnx.commit()
+cursor.execute("SELECT * FROM produtos")
+resultados = cursor.fetchall()
 
+for linha in resultados:
+    print(linha)
+print(f"{cursor.rowcount} registros inseridos.")
 cursor.close()
 cnx.close()
