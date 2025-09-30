2710efd9b0e1e6cbaa73356eeb79b06eb2e1d2a4
Fontes762 <pedrotelheirofontes@gmail.com>
2025-09-30 11:03:33 -0300

    Update produtos_db.sql

--- produtos_db.sql
+++ produtos_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 12-Set-2025 às 04:21
+-- Generation Time: 30-Set-2025 às 15:41
 -- Versão do servidor: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -23,34 +23,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `mercados`
---
-
-CREATE TABLE IF NOT EXISTS `mercados` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nome` (`nome`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `produtos`
 --
 
 CREATE TABLE IF NOT EXISTS `produtos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
-  `preco` decimal(10,2) NOT NULL,
-  `mercado_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `preco` varchar(20) NOT NULL,
+  `mercado` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_nome` (`nome`),
-  KEY `idx_preco` (`preco`),
-  KEY `idx_mercado` (`mercado_id`)
+  KEY `idx_preco` (`preco`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
